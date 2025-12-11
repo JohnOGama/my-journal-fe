3244355dc4c26c01569df51952f1f4b68fbd9d36
JohnOgama <jogama696@gmail.com>
2025-12-11 12:09:54 +0800

    proxy revert

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,24 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
-import { authClient } from "./libs/authClient";
+import { NextRequest } from "next/server";
 
-function isSessionExpired(expiresAt: Date | string | undefined): boolean {
-  if (!expiresAt) return true;
+export function proxy(request: NextRequest) {
+  const sessionToken = request.cookies.get("MJ-token.session_token");
+  const sessionData = request.cookies.get("MJ-token.session_data");
 
-  const expirationDate =
-    typeof expiresAt === "string" ? new Date(expiresAt) : expiresAt;
-
-  return expirationDate.getTime() < Date.now();
-}
-
-export async function proxy(request: NextRequest) {
-  const sessionStatus = await authClient.getSession();
-  const session = sessionStatus?.data?.session;
-  const user = sessionStatus?.data?.user;
-
-  // Check if user exists and session is not expired
-  if (!user || !session || isSessionExpired(session.expiresAt)) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  return NextResponse.next();
+  console.log("tokens", {
+    sessionToken,
+    sessionData,
+  });
 }
